7459474a5d59f3ec3090b7ab1fe02da29a2c3816
Michael Froehlich <m.froehlich@tum.de>
2018-09-26 15:34:55 +0200

    Optimize for other display sizes

--- Noise/Noise/Presentation/OnboardingScene/OnboardingViewController.swift
+++ Noise/Noise/Presentation/OnboardingScene/OnboardingViewController.swift
@@ -14,11 +14,48 @@ class OnboardingViewController: UIViewController  {
     
     @IBOutlet var nextButton: PrimaryButton!
     
+    private var hasRezised: Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         nextButton.backgroundColor = Injection.theme.tintColor
         nextButton.addTarget(self, action: #selector(OnboardingViewController.nextButtonPressed), for: .touchUpInside)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        resizeForIphoneSE()
+    }
+    
+    private func resizeForIphoneSE() {
+        if hasRezised {
+            return
+        }
+        
+        hasRezised =  true
+        let width = UIScreen.main.bounds.width
+        if width > 320 {
+            return
+        }
+        
+        resizeView(view)
+    }
+    
+    private func resizeView(_ view: UIView) {
+        if let label = view as? UILabel {
+            let fontsize = label.font.pointSize - 2.5
+            label.font = label.font.withSize(fontsize)
+        }
+        if let stackView = view as? UIStackView {
+            let spacing = stackView.spacing - 12.0
+            stackView.spacing = spacing
+        }
+        
+        for subview in view.subviews {
+            resizeView(subview)
+        }
     }
     
     @objc func nextButtonPressed(_ sender: Any) {
